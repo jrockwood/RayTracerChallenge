ad6189a72c4b181cd6ecfb3a4fa97bbc8c249071
Justin Rockwood <justinrockwood@hotmail.com>
2020-10-01 23:49:49 -0700

    tuple(), point(), and vector()

--- src/tuples.ts
+++ src/tuples.ts
@@ -1,3 +1,26 @@
-export function add(x: number, y: number): number {
-  return x + y;
+export interface Tuple {
+  readonly x: number;
+  readonly y: number;
+  readonly z: number;
+  readonly w: number;
+}
+
+export function tuple(x: number, y: number, z: number, w: number): Tuple {
+  return { x, y, z, w };
+}
+
+export function point(x: number, y: number, z: number): Tuple {
+  return { x, y, z, w: 1 };
+}
+
+export function vector(x: number, y: number, z: number): Tuple {
+  return { x, y, z, w: 0 };
+}
+
+export function isPoint(tuple: Tuple): boolean {
+  return tuple.w === 1;
+}
+
+export function isVector(tuple: Tuple): boolean {
+  return tuple.w === 0;
 }
--- test/tuples.test.ts
+++ test/tuples.test.ts
@@ -1,7 +1,47 @@
-import { add } from '../src/tuples';
+import { areTuplesEqual, isPoint, isVector, point, tuple, vector } from '../src/tuples';
 
-describe('add', () => {
-  it('should add two numbers', () => {
-    expect(add(1, 3)).toBe(4);
+describe('tuple()', () => {
+  it('should store x, y, z, and w', () => {
+    const point = tuple(4.3, -4.2, 3.1, 1.0);
+    expect(point.x).toBe(4.3);
+    expect(point.y).toBe(-4.2);
+    expect(point.z).toBe(3.1);
+    expect(point.w).toBe(1.0);
+  });
+
+  describe('with w=1.0', () => {
+    const point = tuple(4.3, -4.2, 3.1, 1.0);
+
+    it('should be a point', () => {
+      expect(isPoint(point)).toBe(true);
+    });
+
+    it('should not be a vector', () => {
+      expect(isVector(point)).toBe(false);
+    });
+  });
+
+  describe('with w=0', () => {
+    const vector = tuple(4.3, -4.2, 3.1, 0.0);
+
+    it('should be a vector', () => {
+      expect(isVector(vector)).toBe(true);
+    });
+
+    it('should not be a point', () => {
+      expect(isPoint(vector)).toBe(false);
+    });
+  });
+});
+
+describe('point()', () => {
+  it('creates tuples with w=1', () => {
+    expect(point(4, -4, 3)).toEqual(tuple(4, -4, 3, 1));
+  });
+});
+
+describe('vector()', () => {
+  it('creates tuples with w=0', () => {
+    expect(vector(4, -4, 3)).toEqual(tuple(4, -4, 3, 0));
   });
 });
