c2451c440fd7f6d1fe1b4d8e836f8bb0c2d7411a
Justin Rockwood <justinrockwood@hotmail.com>
2020-10-02 19:51:15 -0700

    Color.add, subtract, and multiply

--- src/Color.ts
+++ src/Color.ts
@@ -1,3 +1,5 @@
+import { floatEqual } from './Math';
+
 export class Color {
   public readonly red: number;
   public readonly green: number;
@@ -7,5 +9,25 @@ export class Color {
     this.red = red;
     this.green = green;
     this.blue = blue;
+  }
+
+  public isEqualTo(other: Color): boolean {
+    return floatEqual(this.red, other.red) && floatEqual(this.green, other.green) && floatEqual(this.blue, other.blue);
+  }
+
+  public add(color: Color): Color {
+    return new Color(this.red + color.red, this.green + color.green, this.blue + color.blue);
+  }
+
+  public subtract(color: Color): Color {
+    return new Color(this.red - color.red, this.green - color.green, this.blue - color.blue);
+  }
+
+  public multiply(other: number | Color): Color {
+    if (typeof other === 'number') {
+      return new Color(this.red * other, this.green * other, this.blue * other);
+    }
+
+    return new Color(this.red * other.red, this.green * other.green, this.blue * other.blue);
   }
 }
--- test/Color.test.ts
+++ test/Color.test.ts
@@ -9,4 +9,33 @@ describe('Color', () => {
       expect(color.blue).toBe(1.7);
     });
   });
+
+  describe('add()', () => {
+    it('should add two colors', () => {
+      const c1 = new Color(0.9, 0.6, 0.75);
+      const c2 = new Color(0.7, 0.1, 0.25);
+      expect(c1.add(c2)).toEqual(new Color(1.6, 0.7, 1.0));
+    });
+  });
+
+  describe('subtract()', () => {
+    it('should subtract two colors', () => {
+      const c1 = new Color(0.9, 0.6, 0.75);
+      const c2 = new Color(0.7, 0.1, 0.25);
+      expect(c1.subtract(c2).isEqualTo(new Color(0.2, 0.5, 0.5))).toBe(true);
+    });
+  });
+
+  describe('multiply()', () => {
+    it('should multiply a color by a scalar', () => {
+      const c = new Color(0.2, 0.3, 0.4);
+      expect(c.multiply(2)).toEqual(new Color(0.4, 0.6, 0.8));
+    });
+
+    it('should multiple two colors', () => {
+      const c1 = new Color(1, 0.2, 0.4);
+      const c2 = new Color(0.9, 1, 0.1);
+      expect(c1.multiply(c2).isEqualTo(new Color(0.9, 0.2, 0.04))).toBe(true);
+    });
+  });
 });
