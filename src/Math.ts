822cb0a6bb68bf2d2f1df68c2c08c41c6ed01834
Justin Rockwood <justinrockwood@hotmail.com>
2020-10-03 21:35:36 -0700

    Add Math.degreesToRadians and unit tests

--- src/Math.ts
+++ src/Math.ts
@@ -5,5 +5,15 @@ export function floatEqual(a: number, b: number): boolean {
 }
 
 export function clamp(x: number, min: number, max: number): number {
+  if (min > max) {
+    throw new Error(`min must be less than or equal to max: min=${min}, max=${max}`);
+  }
+
   return Math.min(max, Math.max(x, min));
+}
+
+const piOver180 = Math.PI / 180.0;
+
+export function degressToRadians(degrees: number): number {
+  return degrees * piOver180;
 }
