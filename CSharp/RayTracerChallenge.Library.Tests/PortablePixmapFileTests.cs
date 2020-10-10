8571818130dfb00d6abf4aa364e4882370569a86
Justin Rockwood <justinrockwood@hotmail.com>
2020-10-10 14:11:30 -0700

    Add PortablePixmapFile class

---
+++ CSharp/RayTracerChallenge.Library.Tests/PortablePixmapFileTests.cs
@@ -0,0 +1,88 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="PortablePixmapFileTests.cs" company="Justin Rockwood">
+//   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
+//   LICENSE.txt in the project root for license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace RayTracerChallenge.Library.Tests
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using FluentAssertions;
+    using NUnit.Framework;
+
+    public class PortablePixmapFileTests
+    {
+        private async Task<string> GetPpmContents(PortablePixmapFile file)
+        {
+            await using var stream = new MemoryStream();
+            await file.SerializeAsync(stream);
+            stream.Position = 0;
+            using var reader = new StreamReader(stream, Encoding.UTF8, leaveOpen: true);
+            string contents = await reader.ReadToEndAsync();
+            return contents;
+        }
+
+        [Test]
+        public async Task Constructing_the_PPM_header()
+        {
+            var file = new PortablePixmapFile(5, 3, Array.Empty<Color>());
+            string contents = await GetPpmContents(file);
+            contents.Split('\n').Take(3).Should().HaveCount(3).And.ContainInOrder("P3", "5 3", "255");
+        }
+
+        [Test]
+        public async Task Constructing_the_PPM_pixel_data()
+        {
+            const int width = 5;
+            const int height = 3;
+
+            var pixels = new Color[width * height];
+            var file = new PortablePixmapFile(width, height, pixels);
+            pixels[0] = new Color(1.5f, 0f, 0f);
+            pixels[(1 * width) + 2] = new Color(0f, 0.5f, 0f);
+            pixels[(2 * width) + 4] = new Color(-0.5f, 0f, 1f);
+
+            string contents = await GetPpmContents(file);
+            contents.Split('\n')
+                .Skip(3)
+                .Should()
+                .HaveCount(2)
+                .And.ContainInOrder(
+                    "255 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 128 0 0 0 0 0 0 0 0 0 0",
+                    "0 0 0 0 0 0 0 0 0 0 0 255");
+        }
+
+        [Test]
+        public async Task Splitting_long_lines_in_PPM_files()
+        {
+            const int width = 10;
+            const int height = 2;
+
+            var pixels = Enumerable.Repeat(new Color(1f, 0.8f, 0.6f), width * height).ToArray();
+            var file = new PortablePixmapFile(10, 2, pixels);
+            string contents = await GetPpmContents(file);
+            contents.Split('\n')
+                .Skip(3)
+                .Should()
+                .HaveCount(4)
+                .And.ContainInOrder(
+                    "255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204",
+                    "153 255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255",
+                    "204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204 153",
+                    "255 204 153 255 204 153 255 204 153");
+        }
+
+        [Test]
+        public async Task PPM_files_are_terminated_by_a_newline_character()
+        {
+            var file = new PortablePixmapFile(5, 3, Array.Empty<Color>());
+            string contents = await GetPpmContents(file);
+            contents.EndsWith('\n');
+        }
+    }
+}
---
+++ CSharp/RayTracerChallenge.Library/PortablePixmapFile.cs
@@ -0,0 +1,131 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="PortablePixmapFile.cs" company="Justin Rockwood">
+//   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
+//   LICENSE.txt in the project root for license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace RayTracerChallenge.Library
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.IO;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Represents a Portable Pixmap (PPM) file, which is a textual representation of a bitmap.
+    /// </summary>
+    public class PortablePixmapFile
+    {
+        //// ===========================================================================================================
+        //// Constructors
+        //// ===========================================================================================================
+
+        public PortablePixmapFile(int width, int height, IReadOnlyList<Color> pixels)
+        {
+            Width = width;
+            Height = height;
+            Pixels = pixels;
+        }
+
+        //// ===========================================================================================================
+        //// Properties
+        //// ===========================================================================================================
+
+        public int Width { get; }
+        public int Height { get; }
+        public IReadOnlyList<Color> Pixels { get; }
+
+        //// ===========================================================================================================
+        //// Methods
+        //// ===========================================================================================================
+
+        public async Task SerializeAsync(Stream stream)
+        {
+            await using var writer = new StreamWriter(
+                stream,
+                new UTF8Encoding(encoderShouldEmitUTF8Identifier: false),
+                leaveOpen: true)
+            {
+                NewLine = "\n"
+            };
+
+            await WriteHeader(writer);
+            await WritePixelData(writer);
+            await writer.FlushAsync();
+        }
+
+        private async Task WriteHeader(TextWriter writer)
+        {
+            // Write the magic number.
+            await writer.WriteLineAsync("P3");
+
+            // Write the width and height.
+            await writer.WriteLineAsync($"{Width} {Height}");
+
+            // Write the maximum color value, inclusive.
+            await writer.WriteLineAsync("255");
+        }
+
+        private async Task WritePixelData(TextWriter writer)
+        {
+            var builder = new LineBuilder();
+            foreach (Color pixel in Pixels)
+            {
+                builder.AppendColor(pixel);
+            }
+
+            string pixelDataLines = builder.ToString();
+            await writer.WriteAsync(pixelDataLines);
+        }
+
+        //// ===========================================================================================================
+        //// Classes
+        //// ===========================================================================================================
+
+        private sealed class LineBuilder
+        {
+            private const int MaxLineLength = 70;
+            private readonly StringBuilder _lineBuilder = new StringBuilder(MaxLineLength + 10);
+            private readonly List<string> _lines = new List<string>();
+
+            public void AppendColor(Color color)
+            {
+                Color scaledColor = color * 255;
+                AppendColorComponent(scaledColor.Red)
+                    .AppendColorComponent(scaledColor.Green)
+                    .AppendColorComponent(scaledColor.Blue);
+            }
+
+            public override string ToString()
+            {
+                return string.Join('\n', _lines) + '\n' + _lineBuilder;
+            }
+
+            private LineBuilder AppendColorComponent(float scaledComponent)
+            {
+                string clamped = Math.Clamp((int)Math.Round(scaledComponent), 0, 255)
+                    .ToString(CultureInfo.InvariantCulture);
+
+                if (_lineBuilder.Length + clamped.Length + 1 > MaxLineLength)
+                {
+                    _lines.Add(_lineBuilder.ToString());
+                    _lineBuilder.Clear().Append(clamped);
+                }
+                else
+                {
+                    if (_lineBuilder.Length > 0)
+                    {
+                        _lineBuilder.Append(" ");
+                    }
+
+                    _lineBuilder.Append(clamped);
+                }
+
+                return this;
+            }
+        }
+    }
+}
