f4de3b1a254a2071f092792308ff848810007f17
Justin Rockwood <justinrockwood@hotmail.com>
2020-10-16 01:32:31 -0700

    Add Observable class

---
+++ CSharp/RayTracerChallenge.App.Library/Observable.cs
@@ -0,0 +1,42 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="Observable.cs" company="Justin Rockwood">
+//   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
+//   LICENSE.txt in the project root for license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace RayTracerChallenge.App.Library
+{
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    public abstract class Observable : INotifyPropertyChanged
+    {
+        //// ===========================================================================================================
+        //// Events
+        //// ===========================================================================================================
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        //// ===========================================================================================================
+        //// Methods
+        //// ===========================================================================================================
+
+        protected bool SetProperty<T>(ref T property, T value, [CallerMemberName] string? propertyName = null)
+        {
+            if (Equals(property, value))
+            {
+                return false;
+            }
+
+            property = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
