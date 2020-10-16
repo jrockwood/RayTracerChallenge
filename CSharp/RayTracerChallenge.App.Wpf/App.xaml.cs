6d8299671f809aaf2ee1e525354f32bd51659b6e
Justin Rockwood <justinrockwood@hotmail.com>
2020-10-15 21:30:22 -0700

    Add WPF app, library, and unit test projects

---
+++ CSharp/RayTracerChallenge.App.Wpf/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="App.xaml.cs" company="Justin Rockwood">
+//   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
+//   LICENSE.txt in the project root for license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace RayTracerChallenge.App.Wpf
+{
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ CSharp/RayTracerChallenge.App.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="MainWindow.xaml.cs" company="Justin Rockwood">
+//   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
+//   LICENSE.txt in the project root for license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace RayTracerChallenge.App.Wpf
+{
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
