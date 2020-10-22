4063dd69ea672ed522fd5f94b7a874d2ced9ea19
Justin Rockwood <justinrockwood@hotmail.com>
2020-10-22 00:49:18 -0700

    Add Pattern class

---
+++ CSharp/RayTracerChallenge.Library.Tests/Patterns/PatternTests.cs
@@ -0,0 +1,76 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="PatternTests.cs" company="Justin Rockwood">
+//   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
+//   LICENSE.txt in the project root for license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace RayTracerChallenge.Library.Tests.Patterns
+{
+    using FluentAssertions;
+    using NUnit.Framework;
+    using RayTracerChallenge.Library.Patterns;
+    using RayTracerChallenge.Library.Shapes;
+
+    public class PatternTests
+    {
+        public class TestPattern : Pattern
+        {
+            public TestPattern(Matrix4x4? transform = null)
+                : base(transform)
+            {
+            }
+
+            public override Color ColorAt(Point point)
+            {
+                return new Color(point.X, point.Y, point.Z);
+            }
+
+            public override Pattern WithTransform(Matrix4x4 value)
+            {
+                return new TestPattern(value);
+            }
+        }
+
+        [Test]
+        public void Ctor_should_default_the_transformation_matrix_to_the_identity_matrix()
+        {
+            var pattern = new TestPattern();
+            pattern.Transform.Should().Be(Matrix4x4.Identity);
+        }
+
+        [Test]
+        public void Ctor_should_store_the_transformation_matrix()
+        {
+            var pattern = new TestPattern(Matrix4x4.CreateTranslation(1, 2, 3));
+            pattern.Transform.Should().Be(Matrix4x4.CreateTranslation(1, 2, 3));
+        }
+
+        [Test]
+        public void ColorOnShapeAt_should_use_the_shape_transformation()
+        {
+            var shape = new Sphere(Matrix4x4.CreateScaling(2, 2, 2));
+            var pattern = new TestPattern();
+            Color color = pattern.ColorOnShapeAt(shape, new Point(2, 3, 4));
+            color.Should().Be(new Color(1, 1.5, 2));
+        }
+
+        [Test]
+        public void ColorOnShapeAt_should_use_the_pattern_transformation()
+        {
+            var shape = new Sphere();
+            var pattern = new TestPattern(Matrix4x4.CreateScaling(2, 2, 2));
+            Color color = pattern.ColorOnShapeAt(shape, new Point(2, 3, 4));
+            color.Should().Be(new Color(1, 1.5, 2));
+        }
+
+        [Test]
+        public void ColorOnShapeAt_should_use_both_the_shape_and_pattern_transformation()
+        {
+            var shape = new Sphere(Matrix4x4.CreateScaling(2, 2, 2));
+            var pattern = new TestPattern(Matrix4x4.CreateTranslation(0.5, 1, 1.5));
+            Color color = pattern.ColorOnShapeAt(shape, new Point(2.5, 3, 3.5));
+            color.Should().Be(new Color(0.75, 0.5, 0.25));
+        }
+    }
+}
---
+++ CSharp/RayTracerChallenge.Library/Patterns/Pattern.cs
@@ -0,0 +1,45 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="Pattern.cs" company="Justin Rockwood">
+//   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
+//   LICENSE.txt in the project root for license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace RayTracerChallenge.Library.Patterns
+{
+    using RayTracerChallenge.Library.Shapes;
+
+    public abstract class Pattern
+    {
+        //// ===========================================================================================================
+        //// Constructors
+        //// ===========================================================================================================
+
+        protected Pattern(Matrix4x4? transform = null)
+        {
+            Transform = transform ?? Matrix4x4.Identity;
+        }
+
+        //// ===========================================================================================================
+        //// Properties
+        //// ===========================================================================================================
+
+        public Matrix4x4 Transform { get; }
+
+        //// ===========================================================================================================
+        //// Methods
+        //// ===========================================================================================================
+
+        public abstract Color ColorAt(Point point);
+
+        public abstract Pattern WithTransform(Matrix4x4 value);
+
+        public Color ColorOnShapeAt(Shape shape, Point worldPoint)
+        {
+            Point shapePoint = shape.Transform.Invert() * worldPoint;
+            Point patternPoint = Transform.Invert() * shapePoint;
+            Color color = ColorAt(patternPoint);
+            return color;
+        }
+    }
+}
