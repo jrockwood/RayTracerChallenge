ff171a05a13925772195c56ce41e01a502c3d22f
Justin Rockwood <justinrockwood@hotmail.com>
2020-10-22 00:49:28 -0700

    Add StripePattern

---
+++ CSharp/RayTracerChallenge.Library.Tests/Patterns/StripePatternTests.cs
@@ -0,0 +1,68 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="StripePatternTests.cs" company="Justin Rockwood">
+//   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
+//   LICENSE.txt in the project root for license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace RayTracerChallenge.Library.Tests.Patterns
+{
+    using FluentAssertions;
+    using NUnit.Framework;
+    using RayTracerChallenge.Library.Patterns;
+
+    public class StripePatternTests
+    {
+        [Test]
+        public void Ctor_should_store_the_two_colors_for_the_stripe()
+        {
+            var pattern = new StripePattern(Colors.White, Colors.Black);
+            pattern.Color1.Should().Be(Colors.White);
+            pattern.Color2.Should().Be(Colors.Black);
+        }
+
+        [Test]
+        public void ColorAt_should_return_the_same_color_for_every_y_coordinate()
+        {
+            var pattern = new StripePattern(Colors.White, Colors.Black);
+            pattern.ColorAt(new Point(0, 0, 0)).Should().Be(Colors.White);
+            pattern.ColorAt(new Point(0, 1, 0)).Should().Be(Colors.White);
+            pattern.ColorAt(new Point(0, 2, 0)).Should().Be(Colors.White);
+        }
+
+        [Test]
+        public void WithX_methods_should_return_a_copy_with_the_value_changed()
+        {
+            var pattern = new StripePattern(Colors.White, Colors.Black);
+            pattern.WithColor1(Colors.Red)
+                .Should()
+                .BeEquivalentTo(new StripePattern(Colors.Red, Colors.Black, Matrix4x4.Identity));
+            pattern.WithColor2(Colors.Green)
+                .Should()
+                .BeEquivalentTo(new StripePattern(Colors.White, Colors.Green, Matrix4x4.Identity));
+            pattern.WithTransform(Matrix4x4.CreateScaling(2, 2, 2))
+                .Should()
+                .BeEquivalentTo(new StripePattern(Colors.White, Colors.Black, Matrix4x4.CreateScaling(2, 2, 2)));
+        }
+
+        [Test]
+        public void ColorAt_should_return_the_same_color_for_every_z_coordinate()
+        {
+            var pattern = new StripePattern(Colors.White, Colors.Black);
+            pattern.ColorAt(new Point(0, 0, 0)).Should().Be(Colors.White);
+            pattern.ColorAt(new Point(0, 0, 1)).Should().Be(Colors.White);
+            pattern.ColorAt(new Point(0, 0, 2)).Should().Be(Colors.White);
+        }
+
+        [Test]
+        public void ColorAt_should_alternate_colors_with_x_coordinates()
+        {
+            var pattern = new StripePattern(Colors.White, Colors.Black);
+            pattern.ColorAt(new Point(0, 0, 0)).Should().Be(Colors.White);
+            pattern.ColorAt(new Point(0.9, 0, 0)).Should().Be(Colors.White);
+            pattern.ColorAt(new Point(1, 0, 0)).Should().Be(Colors.Black);
+            pattern.ColorAt(new Point(-0.1, 0, 0)).Should().Be(Colors.Black);
+            pattern.ColorAt(new Point(-1.1, 0, 0)).Should().Be(Colors.White);
+        }
+    }
+}
---
+++ CSharp/RayTracerChallenge.Library/Patterns/StripePattern.cs
@@ -0,0 +1,56 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="StripePattern.cs" company="Justin Rockwood">
+//   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
+//   LICENSE.txt in the project root for license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace RayTracerChallenge.Library.Patterns
+{
+    using System;
+
+    public class StripePattern : Pattern
+    {
+        //// ===========================================================================================================
+        //// Constructors
+        //// ===========================================================================================================
+
+        public StripePattern(Color color1, Color color2, Matrix4x4? transform = null)
+            : base(transform)
+        {
+            Color1 = color1;
+            Color2 = color2;
+        }
+
+        //// ===========================================================================================================
+        //// Properties
+        //// ===========================================================================================================
+
+        public Color Color1 { get; }
+        public Color Color2 { get; }
+
+        public StripePattern WithColor1(Color value)
+        {
+            return new StripePattern(value, Color2, Transform);
+        }
+
+        public StripePattern WithColor2(Color value)
+        {
+            return new StripePattern(Color1, value, Transform);
+        }
+
+        public override Pattern WithTransform(Matrix4x4 value)
+        {
+            return new StripePattern(Color1, Color2, value);
+        }
+
+        //// ===========================================================================================================
+        //// Methods
+        //// ===========================================================================================================
+
+        public override Color ColorAt(Point point)
+        {
+            return Math.Floor(point.X) % 2 == 0 ? Color1 : Color2;
+        }
+    }
+}
