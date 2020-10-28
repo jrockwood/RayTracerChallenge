509c2e28a9abfe39d1143e352f7e4bdceba0ce33
Justin Rockwood <justinrockwood@hotmail.com>
2020-10-28 01:10:42 -0700

    Add Cylinder

---
+++ CSharp/RayTracerChallenge.Library.Tests/Shapes/CylinderTests.cs
@@ -0,0 +1,178 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="CylinderTests.cs" company="Justin Rockwood">
+//   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
+//   LICENSE.txt in the project root for license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace RayTracerChallenge.Library.Tests.Shapes
+{
+    using System.Linq;
+    using FluentAssertions;
+    using NUnit.Framework;
+    using RayTracerChallenge.Library.Shapes;
+
+    public class CylinderTests
+    {
+        [Test]
+        public void A_ray_misses_a_cylinder()
+        {
+            var testCases = new[]
+            {
+                // Sits on the surface of the cylinder and points along the +y axis.
+                new Ray(new Point(1, 0, 0), Vector.UnitY),
+
+                // Sits inside the cylinder and point along the +y axis.
+                new Ray(new Point(0, 0, 0), Vector.UnitY),
+
+                // Positioned outside the cylinder and oriented askew from all axes.
+                new Ray(new Point(0, 0, -5), new Vector(1, 1, 1)),
+            };
+
+            foreach (Ray testCase in testCases)
+            {
+                var cylinder = new Cylinder();
+                IntersectionList intersections = cylinder.LocalIntersect(testCase);
+                intersections.Should().BeEmpty();
+            }
+        }
+
+        [Test]
+        public void A_ray_strikes_a_cylinder()
+        {
+            var testCases = new[]
+            {
+                // Strikes the cylinder on a tangent.
+                (origin: new Point(1, 0, -5), direction: Vector.UnitZ, t1: 5, t2: 5),
+
+                // Intersects the cylinder perpendicularly through the middle.
+                (origin: new Point(0, 0, -5), direction: Vector.UnitZ, t1: 4, t2: 6),
+
+                // Skewed so that the ray strikes the cylinder at an angle.
+                (origin: new Point(0.5, 0, -5), direction: new Vector(0.1, 1, 1), t1: 6.80798, t2: 7.08872),
+            };
+
+            foreach ((Point origin, Vector vector, double t1, double t2) in testCases)
+            {
+                var cylinder = new Cylinder();
+                var direction = vector.Normalize();
+                var ray = new Ray(origin, direction);
+                IntersectionList intersections = cylinder.LocalIntersect(ray);
+                intersections.Ts.Select(x => x.RoundToEpsilon()).Should().HaveCount(2).And.ContainInOrder(t1, t2);
+            }
+        }
+
+        [Test]
+        public void Normal_vector_on_a_cylinder()
+        {
+            var testCases = new[]
+            {
+                (point: new Point(1, 0, 0), normal: new Vector(1, 0, 0)),
+                (point: new Point(0, 5, -1), normal: new Vector(0, 0, -1)),
+                (point: new Point(0, -2, 1), normal: new Vector(0, 0, 1)),
+                (point: new Point(-1, 1, 0), normal: new Vector(-1, 0, 0)),
+            };
+
+            foreach ((Point point, Vector vector) in testCases)
+            {
+                var cylinder = new Cylinder();
+                Vector normal = cylinder.LocalNormalAt(point);
+                normal.Should().Be(vector);
+            }
+        }
+
+        [Test]
+        public void The_default_minimum_and_maximum_for_a_cylinder()
+        {
+            var cylinder = new Cylinder();
+            cylinder.MinimumY.Should().Be(double.NegativeInfinity);
+            cylinder.MaximumY.Should().Be(double.PositiveInfinity);
+        }
+
+        [Test]
+        public void Intersecting_a_constrained_cylinder()
+        {
+            var testCases = new[]
+            {
+                // Cast a ray diagonally from inside the cylinder, with the ray escaping without intersecting.
+                (point: new Point(0, 1.5, 0), direction: new Vector(0.1, 1, 0), count: 0),
+
+                // Cast a ray perpendicular to the y axis, but from above and below the cylinder.
+                (point: new Point(0, 3, -5), direction: new Vector(0, 0, 1), count: 0),
+                (point: new Point(0, 0, -5), direction: new Vector(0, 0, 1), count: 0),
+
+                // Cast rays perpendicular to the y axis, but at exactly the minimum and maximum.
+                (point: new Point(0, 2, -5), direction: new Vector(0, 0, 1), count: 0),
+                (point: new Point(0, 1, -5), direction: new Vector(0, 0, 1), count: 0),
+
+                // Cast a perpendicular ray through the middle and get two intersections.
+                (point: new Point(0, 1.5, -2), direction: new Vector(0, 0, 1), count: 2),
+            };
+
+            foreach ((Point point, Vector vector, int count) in testCases)
+            {
+                var cylinder = new Cylinder(minimumY: 1, maximumY: 2);
+                var direction = vector.Normalize();
+                var ray = new Ray(point, direction);
+                IntersectionList intersections = cylinder.LocalIntersect(ray);
+                intersections.Should().HaveCount(count);
+            }
+        }
+
+        [Test]
+        public void The_default_closed_value_for_a_cylinder()
+        {
+            var cylinder = new Cylinder();
+            cylinder.IsClosed.Should().BeFalse();
+        }
+
+        [Test]
+        public void Intersecting_the_caps_of_a_closed_cylinder()
+        {
+            var testCases = new[]
+            {
+                // The ray starts above the cylinder and points down through the middle, along the y axis.
+                (point: new Point(0, 3, 0), direction: new Vector(0, -1, 0), count: 2),
+
+                // The ray starts above and below the cylinder and casts through it, intersecting an end cap and side.
+                (point: new Point(0, 3, -2), direction: new Vector(0, -1, 2), count: 2),
+                (point: new Point(0, 0, -2), direction: new Vector(0, 1, 2), count: 2),
+
+                // Corner cases: the ray originates above and below the cylinder, intersecting and end cap, but they
+                // exit the cylinder at the point where the other end cap intersects the side of the cylinder.
+                (point: new Point(0, 4, -2), direction: new Vector(0, -1, 1), count: 2),
+                (point: new Point(0, -1, -2), direction: new Vector(0, 1, 1), count: 2),
+            };
+
+            foreach ((Point point, Vector vector, int count) in testCases)
+            {
+                var cylinder = new Cylinder(minimumY: 1, maximumY: 2, isClosed: true);
+                var direction = vector.Normalize();
+                var ray = new Ray(point, direction);
+                IntersectionList intersections = cylinder.LocalIntersect(ray);
+                intersections.Should().HaveCount(count);
+            }
+        }
+
+        [Test]
+        public void The_normal_vector_on_a_cylinder_s_end_caps()
+        {
+            var testCases = new[]
+            {
+                (point: new Point(0, 1, 0), normal: new Vector(0, -1, 0)),
+                (point: new Point(0.5, 1, 0), normal: new Vector(0, -1, 0)),
+                (point: new Point(0, 1, 0.5), normal: new Vector(0, -1, 0)),
+                (point: new Point(0, 2, 0), normal: new Vector(0, 1, 0)),
+                (point: new Point(0.5, 2, 0), normal: new Vector(0, 1, 0)),
+                (point: new Point(0, 2, 0.5), normal: new Vector(0, 1, 0)),
+            };
+
+            foreach ((Point point, Vector expectedNormal) in testCases)
+            {
+                var cylinder = new Cylinder(minimumY: 1, maximumY: 2, isClosed: true);
+                var normal = cylinder.LocalNormalAt(point);
+                normal.Should().Be(expectedNormal);
+            }
+        }
+    }
+}
---
+++ CSharp/RayTracerChallenge.Library/Shapes/Cylinder.cs
@@ -0,0 +1,162 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="Cylinder.cs" company="Justin Rockwood">
+//   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
+//   LICENSE.txt in the project root for license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace RayTracerChallenge.Library.Shapes
+{
+    using System;
+
+    public class Cylinder : Shape
+    {
+        //// ===========================================================================================================
+        //// Constructors
+        //// ===========================================================================================================
+
+        public Cylinder(
+            double minimumY,
+            double maximumY,
+            bool isClosed = false,
+            Matrix4x4? transform = null,
+            Material? material = null,
+            bool hideShadow = false)
+            : base(transform, material, hideShadow)
+        {
+            MinimumY = minimumY;
+            MaximumY = maximumY;
+            IsClosed = isClosed;
+        }
+
+        public Cylinder(Matrix4x4? transform = null, Material? material = null, bool hideShadow = false)
+            : this(double.NegativeInfinity, double.PositiveInfinity, false, transform, material, hideShadow)
+        {
+        }
+
+        //// ===========================================================================================================
+        //// Properties
+        //// ===========================================================================================================
+
+        public double MinimumY { get; }
+        public double MaximumY { get; }
+
+        public bool IsClosed { get; }
+
+        //// ===========================================================================================================
+        //// Methods
+        //// ===========================================================================================================
+
+        public override Shape WithTransform(Matrix4x4 value)
+        {
+            return new Cylinder(value, Material, HideShadow);
+        }
+
+        public override Shape WithMaterial(Material value)
+        {
+            return new Cylinder(Transform, value, HideShadow);
+        }
+
+        protected internal override IntersectionList LocalIntersect(Ray localRay)
+        {
+            double a = (localRay.Direction.X * localRay.Direction.X) + (localRay.Direction.Z * localRay.Direction.Z);
+
+            // Ray is parallel to the y axis.
+            if (a.IsApproximatelyEqual(0))
+            {
+                return IntersectCaps(localRay, IntersectionList.Empty);
+            }
+
+            double b = (2 * localRay.Origin.X * localRay.Direction.X) + (2 * localRay.Origin.Z * localRay.Direction.Z);
+            double c = ((localRay.Origin.X * localRay.Origin.X) + (localRay.Origin.Z * localRay.Origin.Z)) - 1;
+            double discriminant = (b * b) - (4 * a * c);
+
+            // Ray does not intersect the cylinder.
+            if (discriminant < 0)
+            {
+                return IntersectionList.Empty;
+            }
+
+            double sqrtDiscriminant = Math.Sqrt(discriminant);
+            double t0 = (-b - sqrtDiscriminant) / (2 * a);
+            double t1 = (-b + sqrtDiscriminant) / (2 * a);
+
+            if (t0 > t1)
+            {
+                double temp = t0;
+                t0 = t1;
+                t1 = temp;
+            }
+
+            var intersections = IntersectionList.Empty;
+
+            double y0 = localRay.Origin.Y + (t0 * localRay.Direction.Y);
+            double y1 = localRay.Origin.Y + (t1 * localRay.Direction.Y);
+
+            if (y0 > MinimumY && y0 < MaximumY)
+            {
+                intersections = intersections.Add((t0, this));
+            }
+
+            if (y1 > MinimumY && y1 < MaximumY)
+            {
+                intersections = intersections.Add((t1, this));
+            }
+
+            intersections = IntersectCaps(localRay, intersections);
+
+            return intersections;
+        }
+
+        protected internal override Vector LocalNormalAt(Point localPoint)
+        {
+            // Compute the square of the distance from the y axis.
+            double distance = (localPoint.X * localPoint.X) + (localPoint.Z * localPoint.Z);
+            if (distance < 1 && localPoint.Y >= MaximumY - NumberExtensions.Epsilon)
+            {
+                return Vector.UnitY;
+            }
+            else if (distance < 1 && localPoint.Y <= MinimumY + NumberExtensions.Epsilon)
+            {
+                return new Vector(0, -1, 0);
+            }
+
+            return new Vector(localPoint.X, 0, localPoint.Z);
+        }
+
+        /// <summary>
+        /// Checks to see if the intersection at `t` is within a radius of 1 (the radius of the cylinder) from the y axis.
+        /// </summary>
+        private bool CheckCapHit(Ray ray, double t)
+        {
+            double x = ray.Origin.X + (t * ray.Direction.X);
+            double z = ray.Origin.Z + (t * ray.Direction.Z);
+            return (x * x) + (z * z) <= 1;
+        }
+
+        private IntersectionList IntersectCaps(Ray ray, IntersectionList intersections)
+        {
+            // Caps only matter if the cylinder is closed, and might possibly be intersected by the ray.
+            if (!IsClosed || ray.Direction.Y.IsApproximatelyEqual(0))
+            {
+                return intersections;
+            }
+
+            // Check for an intersection with the lower end cap by intersecting the ray with the plane at MinimumY.
+            double t = (MinimumY - ray.Origin.Y) / ray.Direction.Y;
+            if (CheckCapHit(ray, t))
+            {
+                intersections = intersections.Add((t, this));
+            }
+
+            // Check for an intersection with the upper end cap by intersecting the ray with the plane at MaximumY.
+            t = (MaximumY - ray.Origin.Y) / ray.Direction.Y;
+            if (CheckCapHit(ray, t))
+            {
+                intersections = intersections.Add((t, this));
+            }
+
+            return intersections;
+        }
+    }
+}
