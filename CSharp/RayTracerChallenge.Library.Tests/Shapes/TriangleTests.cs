c6d93483128d35b566651f9664e21cddc4144bd1
Justin Rockwood <justinrockwood@hotmail.com>
2020-11-06 17:56:35 -0800

    Add Triangle

---
+++ CSharp/RayTracerChallenge.Library.Tests/Shapes/TriangleTests.cs
@@ -0,0 +1,89 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="TriangleTests.cs" company="Justin Rockwood">
+//   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
+//   LICENSE.txt in the project root for license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace RayTracerChallenge.Library.Tests.Shapes
+{
+    using FluentAssertions;
+    using NUnit.Framework;
+    using RayTracerChallenge.Library.Shapes;
+
+    public class TriangleTests
+    {
+        [Test]
+        public void Constructing_a_triangle()
+        {
+            var p1 = new Point(0, 1, 0);
+            var p2 = new Point(-1, 0, 0);
+            var p3 = new Point(1, 0, 0);
+            var triangle = new Triangle(p1, p2, p3);
+            triangle.P1.Should().Be(p1);
+            triangle.P2.Should().Be(p2);
+            triangle.P3.Should().Be(p3);
+
+            triangle.E1.Should().Be(new Vector(-1, -1, 0));
+            triangle.E2.Should().Be(new Vector(1, -1, 0));
+            triangle.Normal.Should().Be(new Vector(0, 0, -1));
+        }
+
+        [Test]
+        public void Finding_the_normal_on_a_triangle()
+        {
+            var triangle = new Triangle(new Point(0, 1, 0), new Point(-1, 0, 0), new Point(1, 0, 0));
+            var n1 = triangle.LocalNormalAt(new Point(0, 0.5, 0));
+            var n2 = triangle.LocalNormalAt(new Point(-0.5, 0.75, 0));
+            var n3 = triangle.LocalNormalAt(new Point(0.5, 0.25, 0));
+            n1.Should().Be(triangle.Normal);
+            n2.Should().Be(triangle.Normal);
+            n3.Should().Be(triangle.Normal);
+        }
+
+        [Test]
+        public void Intersecting_a_ray_parallel_to_the_triangle()
+        {
+            var triangle = new Triangle(new Point(0, 1, 0), new Point(-1, 0, 0), new Point(1, 0, 0));
+            var ray = new Ray(new Point(0, -1, -2), new Vector(0, 1, 0));
+            var intersections = triangle.LocalIntersect(ray);
+            intersections.Should().BeEmpty();
+        }
+
+        [Test]
+        public void A_ray_misses_the_p1_p3_edge()
+        {
+            var triangle = new Triangle(new Point(0, 1, 0), new Point(-1, 0, 0), new Point(1, 0, 0));
+            var ray = new Ray(new Point(1, 1, -2), new Vector(0, 0, 1));
+            var intersections = triangle.LocalIntersect(ray);
+            intersections.Should().BeEmpty();
+        }
+
+        [Test]
+        public void A_ray_misses_the_p1_p2_edge()
+        {
+            var triangle = new Triangle(new Point(0, 1, 0), new Point(-1, 0, 0), new Point(1, 0, 0));
+            var ray = new Ray(new Point(-1, 1, -2), new Vector(0, 0, 1));
+            var intersections = triangle.LocalIntersect(ray);
+            intersections.Should().BeEmpty();
+        }
+
+        [Test]
+        public void A_ray_misses_the_p2_p3_edge()
+        {
+            var triangle = new Triangle(new Point(0, 1, 0), new Point(-1, 0, 0), new Point(1, 0, 0));
+            var ray = new Ray(new Point(0, -1, -2), new Vector(0, 0, 1));
+            var intersections = triangle.LocalIntersect(ray);
+            intersections.Should().BeEmpty();
+        }
+
+        [Test]
+        public void A_ray_strikes_a_triangle()
+        {
+            var triangle = new Triangle(new Point(0, 1, 0), new Point(-1, 0, 0), new Point(1, 0, 0));
+            var ray = new Ray(new Point(0, 0.5, -2), new Vector(0, 0, 1));
+            var intersections = triangle.LocalIntersect(ray);
+            intersections.Ts.Should().HaveCount(1).And.ContainInOrder(2);
+        }
+    }
+}
---
+++ CSharp/RayTracerChallenge.Library/Shapes/Triangle.cs
@@ -0,0 +1,90 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="Triangle.cs" company="Justin Rockwood">
+//   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
+//   LICENSE.txt in the project root for license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace RayTracerChallenge.Library.Shapes
+{
+    public class Triangle : Shape
+    {
+        //// ===========================================================================================================
+        //// Constructors
+        //// ===========================================================================================================
+
+        public Triangle(Point p1, Point p2, Point p3, Matrix4x4? transform = null, Material? material = null)
+            : base(transform, material)
+        {
+            P1 = p1;
+            P2 = p2;
+            P3 = p3;
+
+            E1 = p2 - p1;
+            E2 = p3 - p1;
+            Normal = E2.Cross(E1).Normalize();
+
+            BoundingBox = new BoundingBox(Point.Zero, Point.Zero);
+        }
+
+        //// ===========================================================================================================
+        //// Properties
+        //// ===========================================================================================================
+
+        public Point P1 { get; }
+        public Point P2 { get; }
+        public Point P3 { get; }
+
+        public Vector E1 { get; }
+        public Vector E2 { get; }
+
+        public Vector Normal { get; }
+
+        public override BoundingBox BoundingBox { get; }
+
+        //// ===========================================================================================================
+        //// Methods
+        //// ===========================================================================================================
+
+        public override string ToString()
+        {
+            return $"P1 = {P1}, P2 = {P2}, P3 = {P3}";
+        }
+
+        protected internal override IntersectionList LocalIntersect(Ray localRay)
+        {
+            Vector directionCrossE2 = localRay.Direction.Cross(E2);
+            double determinant = E1.Dot(directionCrossE2);
+
+            if (determinant.IsApproximatelyEqual(0))
+            {
+                return IntersectionList.Empty;
+            }
+
+            double f = 1.0 / determinant;
+            Vector p1ToOrigin = localRay.Origin - P1;
+            double u = f * p1ToOrigin.Dot(directionCrossE2);
+
+            if (u < 0 || u > 1)
+            {
+                return IntersectionList.Empty;
+            }
+
+            Vector originCrossE1 = p1ToOrigin.Cross(E1);
+            double v = f * localRay.Direction.Dot(originCrossE1);
+
+            if (v < 0 || u + v > 1)
+            {
+                return IntersectionList.Empty;
+            }
+
+            double t = f * E2.Dot(originCrossE1);
+            return new IntersectionList(new Intersection(t, this));
+        }
+
+        protected internal override Vector LocalNormalAt(Point localPoint)
+        {
+            return Normal;
+        }
+    }
+}
