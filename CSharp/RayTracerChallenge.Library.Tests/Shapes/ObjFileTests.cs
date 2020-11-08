1fbf06af985145b9d5cc80464e02d59d08937049
Justin Rockwood <justinrockwood@hotmail.com>
2020-11-08 10:42:30 -0800

    Add ObjFile

---
+++ CSharp/RayTracerChallenge.Library.Tests/Shapes/ObjFileTests.cs
@@ -0,0 +1,103 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="ObjFileTests.cs" company="Justin Rockwood">
+//   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
+//   LICENSE.txt in the project root for license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace RayTracerChallenge.Library.Tests.Shapes
+{
+    using FluentAssertions;
+    using NUnit.Framework;
+    using RayTracerChallenge.Library.Shapes;
+
+    public class ObjFileTests
+    {
+        [Test]
+        public void Parse_should_silently_ignore_any_unrecognized_statements()
+        {
+            const string gibberish = @"
+There was a young lady named Bright
+who traveled much faster than light.
+She set out one day
+in a relative way,
+and came back the previous night.";
+
+            var objFile = ObjFile.Parse(gibberish);
+            objFile.IgnoredLineCount.Should().Be(5);
+        }
+
+        [Test]
+        public void Parse_should_process_vertex_data_from_the_given_input()
+        {
+            const string file = @"v -1 1 0
+v -1.0000 0.5000 0.0000
+v 1 0 0
+v 1 1 0";
+
+            var objFile = ObjFile.Parse(file);
+            objFile.Vertices.Should()
+                .HaveCount(4)
+                .And.ContainInOrder(
+                    new Point(-1, 1, 0),
+                    new Point(-1, 0.5, 0),
+                    new Point(1, 0, 0),
+                    new Point(1, 1, 0));
+        }
+
+        [Test]
+        public void Parse_should_process_triangle_data_from_the_given_input()
+        {
+            const string file = @"v -1 1 0
+v -1 0 0
+v 1 0 0
+v 1 1 0
+
+f 1 2 3
+f 1 3 4";
+
+            var objFile = ObjFile.Parse(file);
+            Group g = objFile.DefaultGroup;
+            var t1 = (Triangle)g.Children[0];
+            var t2 = (Triangle)g.Children[1];
+
+            t1.P1.Should().Be(objFile.Vertices[0]);
+            t1.P2.Should().Be(objFile.Vertices[1]);
+            t1.P3.Should().Be(objFile.Vertices[2]);
+
+            t2.P1.Should().Be(objFile.Vertices[0]);
+            t2.P2.Should().Be(objFile.Vertices[2]);
+            t2.P3.Should().Be(objFile.Vertices[3]);
+        }
+
+        [Test]
+        public void Parse_should_process_and_triangulate_polygonal_data_from_the_given_input()
+        {
+            const string file = @"v -1 1 0
+v -1 0 0
+v 1 0 0
+v 1 1 0
+v 0 2 0
+
+f 1 2 3 4 5";
+
+            var objFile = ObjFile.Parse(file);
+            Group g = objFile.DefaultGroup;
+            var t1 = (Triangle)g.Children[0];
+            var t2 = (Triangle)g.Children[1];
+            var t3 = (Triangle)g.Children[2];
+
+            t1.P1.Should().Be(objFile.Vertices[0]);
+            t1.P2.Should().Be(objFile.Vertices[1]);
+            t1.P3.Should().Be(objFile.Vertices[2]);
+
+            t2.P1.Should().Be(objFile.Vertices[0]);
+            t2.P2.Should().Be(objFile.Vertices[2]);
+            t2.P3.Should().Be(objFile.Vertices[3]);
+
+            t3.P1.Should().Be(objFile.Vertices[0]);
+            t3.P2.Should().Be(objFile.Vertices[3]);
+            t3.P3.Should().Be(objFile.Vertices[4]);
+        }
+    }
+}
---
+++ CSharp/RayTracerChallenge.Library/Shapes/ObjFile.cs
@@ -0,0 +1,108 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="ObjFile.cs" company="Justin Rockwood">
+//   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
+//   LICENSE.txt in the project root for license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace RayTracerChallenge.Library.Shapes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+
+    public class ObjFile
+    {
+        //// ===========================================================================================================
+        //// Constructors
+        //// ===========================================================================================================
+
+        private ObjFile(int ignoredLineCount, IReadOnlyList<Point> vertices, Group defaultGroup)
+        {
+            IgnoredLineCount = ignoredLineCount;
+            Vertices = vertices;
+            DefaultGroup = defaultGroup;
+        }
+
+        //// ===========================================================================================================
+        //// Properties
+        //// ===========================================================================================================
+
+        public int IgnoredLineCount { get; }
+
+        public IReadOnlyList<Point> Vertices { get; }
+
+        public Group DefaultGroup { get; }
+
+        //// ===========================================================================================================
+        //// Methods
+        //// ===========================================================================================================
+
+        public static ObjFile Parse(string contents)
+        {
+            int ignoredLines = 0;
+            var vertices = new List<Point>();
+            var group = new Group();
+
+            string[] lines = contents.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string line in lines.Where(s => !string.IsNullOrWhiteSpace(s)))
+            {
+                switch (line.Substring(0, 2))
+                {
+                    case "v ":
+                        vertices.Add(ParsePoint(line.Substring(2)));
+                        break;
+
+                    case "f ":
+                        ParseTriangulatedFace(line.Substring(2), vertices, group);
+                        break;
+
+                    default:
+                        ignoredLines++;
+                        break;
+                }
+            }
+
+            return new ObjFile(ignoredLines, vertices, group);
+        }
+
+        private static Point ParsePoint(string contents)
+        {
+            string[] points = contents.Split(' ');
+            if (points.Length < 3)
+            {
+                throw new InvalidOperationException();
+            }
+
+            double p1 = double.Parse(points[0], CultureInfo.InvariantCulture);
+            double p2 = double.Parse(points[1], CultureInfo.InvariantCulture);
+            double p3 = double.Parse(points[2], CultureInfo.InvariantCulture);
+
+            return new Point(p1, p2, p3);
+        }
+
+        private static void ParseTriangulatedFace(string contents, IReadOnlyList<Point> vertices, Group group)
+        {
+            string[] faceIndexStrings = contents.Split(' ');
+            if (faceIndexStrings.Length < 3)
+            {
+                throw new InvalidOperationException();
+            }
+
+            // Face indexes are all 1-based; convert to 0-based.
+            int[] faceIndexes = faceIndexStrings.Select(x => int.Parse(x, CultureInfo.InvariantCulture) - 1).ToArray();
+
+            for (int i = 1; i < faceIndexes.Length - 1; i++)
+            {
+                var triangle = new Triangle(
+                    vertices[faceIndexes[0]],
+                    vertices[faceIndexes[i]],
+                    vertices[faceIndexes[i + 1]]);
+
+                group.AddChild(triangle);
+            }
+        }
+    }
+}
